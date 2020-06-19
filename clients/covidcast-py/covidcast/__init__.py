54cb0c0aff376aa3442ca00cddb426b103495dba
Alex Reinhart <alex@refsmmat.com>
2020-06-19 10:12:28 -0400

    Provide a package docstring

--- clients/covidcast-py/covidcast/__init__.py
+++ clients/covidcast-py/covidcast/__init__.py
@@ -1,1 +1,14 @@
+"""Fetch data from Delphi's COVIDcast API.
+
+The COVIDcast API provides daily updated data on the COVID-19 pandemic in the
+United States, including cases, deaths, medical records, nationwide symptom
+surveys, and other data collated by the Delphi research group at Carnegie Mellon
+University.
+
+Functions:
+
+* signal - Fetch a Pandas data frame for one signal.
+
+"""
+
 from .covidcast import signal

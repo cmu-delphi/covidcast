7c6545d62558939706ac5b6103d4387935fa7909
Ryan Tibshirani <ryantibs@gmail.com>
2020-08-30 23:46:15 -0400

    Include parent states

--- R-packages/data-raw/make.R
+++ R-packages/data-raw/make.R
@@ -1,21 +1,22 @@
 format_fips <- function(fips) { sprintf("%05d", fips) }
 
-## County census data
+# County census data
 county_census = read.csv("https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv", stringsAsFactors = FALSE)
 county_census$FIPS = format_fips(county_census$STATE * 1000 + county_census$COUNTY)
 
 # County names are in latin1; convert to UTF-8
-county_census$CTYNAME <- iconv(county_census$CTYNAME, "latin1", "UTF-8")
-Encoding(county_census$CTYNAME) <- "UTF-8"
+county_census$CTYNAME = iconv(county_census$CTYNAME, "latin1", "UTF-8")
+Encoding(county_census$CTYNAME) = "UTF-8"
 save(county_census, file = "../covidcast/data/county_census.rda", compress = "bzip2")
 
-## MSA census data
+# MSA census data
 msa_census = read.csv("https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/metro/totals/cbsa-est2019-alldata.csv", stringsAsFactors = FALSE)
 msa_census$CBSA = as.character(msa_census$CBSA)
 
 # Convert names to UTF-8
-msa_census$NAME <- iconv(msa_census$NAME, "latin1", "UTF-8")
-Encoding(msa_census$NAME) <- "UTF-8"
+msa_census$NAME = iconv(msa_census$NAME, "latin1", "UTF-8")
+Encoding(msa_census$NAME) = "UTF-8"
+msa_census$STATE = substring(msa_census$NAME, regexpr(",", msa_census$NAME) + 2)
 save(msa_census, file = "../covidcast/data/msa_census.rda")
 
 # State census data

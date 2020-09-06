d36777e0ee602c1bc6fe573c6e50874ad0d0ad63
chinandrew <chinandrew96@gmail.com>
2020-09-06 01:56:55 -0700

    parametrize tests

--- Python-packages/covidcast-py/tests/covidcast/test_geography.py
+++ Python-packages/covidcast-py/tests/covidcast/test_geography.py
@@ -3,84 +3,227 @@ import pytest
 from covidcast import geography
 
 
-def test_fips_to_name():
-    assert geography.fips_to_name("not a fips") == {}
-    assert geography.fips_to_name("42003") == {"42003": "Allegheny County"}
-    assert geography.fips_to_name("4200", ties_method="all") == [{"42000": ["Pennsylvania"],
-                                                                  "42001": ["Adams County"],
-                                                                  "42003": ["Allegheny County"],
-                                                                  "42005": ["Armstrong County"],
-                                                                  "42007": ["Beaver County"],
-                                                                  "42009": ["Bedford County"]}]
+@pytest.mark.parametrize("test_key, test_kwargs, expected", [
+    (
+            "not a fips",
+            {},
+            {}
+    ),
+    (
+            "42003",
+            {},
+            {"42003": "Allegheny County"}),
+    (
+            "4200",
+            {"ties_method": "all"},
+            [{"42000": ["Pennsylvania"], "42001": ["Adams County"],
+              "42003": ["Allegheny County"], "42005": ["Armstrong County"],
+              "42007": ["Beaver County"], "42009": ["Bedford County"]}]
+    ),
+    (
+            "4200",
+            {},
+            {"42000": "Pennsylvania"}
+    )
+])
+def test_fips_to_name(test_key, test_kwargs, expected):
+    assert geography.fips_to_name(test_key, **test_kwargs) == expected
 
 
-def test_cbsa_to_name():
-    assert geography.cbsa_to_name("38300") == {"38300": "Pittsburgh, PA"}
-    assert geography.cbsa_to_name("not a cbsa") == {}
+@pytest.mark.parametrize("test_key, test_kwargs, expected", [
+    (
+            "not a cbsa",
+            {},
+            {}
+    ),
+    (
+            "38300",
+            {},
+            {"38300": "Pittsburgh, PA"}
+    ),
+])
+def test_cbsa_to_name(test_key, test_kwargs, expected):
+    assert geography.cbsa_to_name(test_key, **test_kwargs) == expected
 
 
-def test_abbr_to_name():
-    assert geography.abbr_to_name("CA") == {"CA": "California"}
-    assert geography.abbr_to_name("CAA") == {}
-    assert geography.abbr_to_name(["CA", "PA"]) == {"CA": "California", "PA": "Pennsylvania"}
-    assert geography.abbr_to_name(["CAAA", "PA"]) == {"PA": "Pennsylvania"}
+@pytest.mark.parametrize("test_key, test_kwargs, expected", [
+    (
+            "CA",
+            {},
+            {"CA": "California"}
+    ),
+    (
+            "CAA",
+            {},
+            {}
+    ),
+    (
+            ["CA",
+             "PA"],
+            {},
+            {"CA": "California", "PA": "Pennsylvania"}
+    ),
+    (
+            ["CAAA", "PA"],
+            {},
+            {"PA": "Pennsylvania"}
+    ),
+])
+def test_abbr_to_name(test_key, test_kwargs, expected):
+    assert geography.abbr_to_name(test_key, **test_kwargs) == expected
 
 
-def test_name_to_abbr():
-    assert geography.name_to_abbr("California") == {"California": "CA"}
-    assert geography.name_to_abbr("Californiaaaaaa") == {}
-    assert geography.name_to_abbr(["California", "Pennsylvania"]) == \
-           {"California": "CA", "Pennsylvania": "PA"}
-    assert geography.name_to_abbr(["California", "Pennsylvaniaa"]) == {"California": "CA"}
+@pytest.mark.parametrize("test_key, test_kwargs, expected", [
+    (
+            "California",
+            {},
+            {"California": "CA"}
+    ),
+    (
+            "Californiaaaaa",
+            {},
+            {}
+    ),
+    (
+            ["California", "Pennsylvania"],
+            {},
+            {"California": "CA", "Pennsylvania": "PA"}
+    ),
+    (
+            ["California", "Pennsylvaniaa"],
+            {},
+            {"California": "CA"}
+    ),
+])
+def test_name_to_abbr(test_key, test_kwargs, expected):
+    assert geography.name_to_abbr(test_key, **test_kwargs) == expected
 
 
-def test_name_to_cbsa():
-    assert geography.name_to_cbsa("Pittsburgh") == {"Pittsburgh, PA": "38300"}
-    assert geography.name_to_cbsa("New", state="CA") == {}
+@pytest.mark.parametrize("test_key, test_kwargs, expected", [
+    (
+            "Pittsburgh",
+            {},
+            {"Pittsburgh, PA": "38300"}
+    ),
+    (
+            "New",
+            {"state": "CA"},
+            {}
+    )
+])
+def test_name_to_cbsa(test_key, test_kwargs, expected):
+    assert geography.name_to_cbsa(test_key, **test_kwargs) == expected
 
 
-def test_name_to_fips():
-    assert geography.name_to_fips("Allegheny") == {"Allegheny County": "42003"}
-    assert geography.name_to_fips("Miami") == {"Miami-Dade County": "12086"}
-    assert geography.name_to_fips("Miami", ties_method="all") == \
-           [{"Miami-Dade County": ["12086"], "Miami County": ["18103", "20121", "39109"]}]
-    assert geography.name_to_fips(["Allegheny", "Miami", "New "], ties_method="all") == \
-           [
+@pytest.mark.parametrize("test_key, test_kwargs, expected", [
+    (
+            "Allegheny",
+            {},
+            {"Allegheny County": "42003"}
+    ),
+    (
+            "Miami",
+            {},
+            {"Miami-Dade County": "12086"}
+    ),
+    (
+            "Miami",
+            {"ties_method": "all"},
+            [{"Miami-Dade County": ["12086"], "Miami County": ["18103", "20121", "39109"]}]
+     ),
+    (
+            ["Allegheny", "Miami", "New "],
+            {"ties_method": "all"},
+            [
                 {"Allegheny County": ["42003"]},
-                {"Miami-Dade County": ["12086"], "Miami County": ["18103", "20121", "39109"]},
+                {"Miami-Dade County": ["12086"],
+                 "Miami County": ["18103", "20121", "39109"]},
                 {"New Haven County": ["09009"], "New London County": ["09011"],
                  "New Castle County": ["10003"], "New Madrid County": ["29143"],
                  "New York County": ["36061"], "New Hanover County": ["37129"],
                  "New Kent County": ["51127"], "New Hampshire": ["33000"],
                  "New Jersey": ["34000"], "New Mexico": ["35000"],
                  "New York": ["36000"]}
-           ]
-    assert geography.name_to_fips(["Allegheny", "Miami", "New "]) == {"Allegheny County": "42003",
-                                                                      "Miami-Dade County": "12086",
-                                                                      "New Haven County": "09009"}
-    assert geography.name_to_fips("New ", ties_method="all", state="ny") == \
-           [{'New York': ['36000'], 'New York County': ['36061']}]
+            ]
+    ),
+    (
+            ["Allegheny", "Miami", "New "],
+            {},
+            {"Allegheny County": "42003", "Miami-Dade County": "12086", "New Haven County": "09009"}
+    ),
+    (
+            "New ",
+            {"ties_method": "all", "state": "ny"},
+            [{"New York": ["36000"], "New York County": ["36061"]}]
+    ),
+])
+def test_name_to_fips(test_key, test_kwargs, expected):
+    assert geography.name_to_fips(test_key, **test_kwargs) == expected
 
 
-def test__lookup():
-    # test invalid ties method
+@pytest.mark.parametrize("test_args, test_kwargs, expected", [
+    (
+            (["a", "b"], ["a", "b", "c"], ["x", "y", "z"]),
+            {},
+            {"a": "x", "b": "y"}
+    ),
+    (
+            (["A"], ["aa", "a"], ["x", "y"]),
+            {},
+            {}
+    ),
+    (
+            (["A"], ["aa", "a"], ["x", "y"]),
+            {"ignore_case": True},
+            {"aa": "x"}
+    ),
+    (
+            (["a"], ["a", "a"], ["x", "y"]),
+            {"ties_method": "all"},
+            [{"a": ["x", "y"]}]
+    ),
+    (
+            (["a"], ["aa", "a"], ["x", "y"]),
+             {"ties_method": "all"},
+            [{"aa": ["x"], "a": ["y"]}]
+    ),
+    (
+            (["a"], ["aa", "a"], ["x", "y"]),
+            {"ties_method": "all", "fixed": True},
+            [{"a": ["y"]}]
+    ),
+    (
+            (["A"], ["aa", "a"], ["x", "y"]),
+            {"ties_method": "all", "fixed": True, "ignore_case": True},
+            []
+    ),
+    (
+            (["A"], ["a", "a"], ["x", "y"]),
+            {"ties_method": "all", "ignore_case": True},
+            [{"a": ["x", "y"]}]
+    )
+])
+def test__lookup(test_args, test_kwargs, expected):
+    assert geography._lookup(*test_args, **test_kwargs) == expected
     with pytest.raises(ValueError):
         geography._lookup(None, None, None, ties_method="not a real method")
-    assert geography._lookup(["a", "b"], ["a", "b", "c"], ["x", "y", "z"]) == {"a": "x", "b": "y"}
-    assert geography._lookup(["A"], ["a", "a"], ["x", "y"]) == {}
-    assert geography._lookup(["A"], ["a", "a"], ["x", "y"], ignore_case=True) == {"a": "x"}
-    assert geography._lookup(["a"], ["a", "a"], ["x", "y"],
-                             ties_method="all") == [{"a": ["x", "y"]}]
-    assert geography._lookup(["A"], ["a", "a"], ["x", "y"],
-                             ignore_case=True, ties_method="all") == [{"a": ["x", "y"]}]
 
 
-def test__get_first_tie():
+@pytest.mark.parametrize("test_dict_list, expected", [
+    (
+            [{"a": ["x", "y"]}],
+            {"a": "x"}
+    ),
+    (
+            [{"a": ["x", "y"], "b": ["i", "j", "k"]}],
+            {"a": "x"}
+    ),
+    (
+            [{"a": ["x", "y"]}, {"b": ["i", "j", "k"]}],
+            {"a": "x", "b": "i"}
+    )
+])
+def test__get_first_tie(test_dict_list, expected):
     with pytest.warns(UserWarning):
-        assert geography._get_first_tie([{"a": ["x", "y"]}]) == {"a": "x"}
-    with pytest.warns(UserWarning):
-        assert geography._get_first_tie([{"a": ["x", "y"], "b": ["i", "j", "k"]}]) == {"a": "x"}
-    with pytest.warns(UserWarning):
-        assert geography._get_first_tie([{"a": ["x", "y"]},
-                                         {"b": ["i", "j", "k"]}]) == {"a": "x", "b": "i"}
+        assert geography._get_first_tie(test_dict_list) == expected
     assert geography._get_first_tie([{"a": ["x"]}]) == {"a": "x"}

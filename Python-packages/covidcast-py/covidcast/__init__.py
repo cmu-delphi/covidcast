866f7c86566f994c91b71464ad4766633fc2b40c
chinandrew <chinandrew96@gmail.com>
2020-09-16 01:20:55 -0700

    Add animation function

--- Python-packages/covidcast-py/covidcast/__init__.py
+++ Python-packages/covidcast-py/covidcast/__init__.py
@@ -13,6 +13,6 @@ Functions:
 """
 
 from .covidcast import signal, metadata
-from .plotting import plot_choropleth, get_geo_df
+from .plotting import plot_choropleth, get_geo_df, animate
 from .geography import (fips_to_name, cbsa_to_name, abbr_to_name,
                         name_to_abbr, name_to_cbsa, name_to_fips)

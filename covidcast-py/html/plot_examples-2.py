6f3ceb2678ac7bae8eb749f7cbd19ebbfda24195
capnrefsmmat <alex@refsmmat.com>
2020-10-01 14:46:00 +0000

    Deploying to gh-pages from  @ d52539b6b059feb6ac1b372160d03683a7b3c66b 🚀

--- covidcast-py/html/plot_examples-2.py
+++ covidcast-py/html/plot_examples-2.py
@@ -1,6 +1,6 @@
 import covidcast
 from datetime import date
 from matplotlib import pyplot as plt
-data = covidcast.signal("fb-survey", "smoothed_cli", start_day = date(2020,8,3), end_day = date(2020,8,4), geo_type = "state")
+data = covidcast.signal("fb-survey", "smoothed_cli", start_day = date(2020,8,4), end_day = date(2020,8,4), geo_type = "state")
 covidcast.plot_choropleth(data)
 plt.show()

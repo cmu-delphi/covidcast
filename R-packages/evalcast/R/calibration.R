0a2d094dfcb78aeeb3381ac6ef6858d60b04761d
Ryan Tibshirani <ryantibs@gmail.com>
2020-10-22 00:55:17 -0400

    Fix logic in assert_that calls

--- R-packages/evalcast/R/calibration.R
+++ R-packages/evalcast/R/calibration.R
@@ -26,7 +26,7 @@ compute_coverage <- function(score_card) {
   assert_that(nrow(score_card) != 0,
               msg="Can't compute coverage for an empty score_card.")
   probs <- score_card$forecast_distribution[[1]]$probs
-  assert_that(length(probs) %% 2 == 1 & all(abs(probs + rev(probs) - 1) < 1e-3),
+  assert_that(length(probs) %% 2 == 1 && all(abs(probs + rev(probs) - 1) < 1e-3),
               msg="Quantile levels need to be symmetric around 0.5 (and include 0.5).")
   num_intervals <- (length(probs) - 1) / 2
   nominal_coverage_probs <- 1 - 2 * probs[1:num_intervals]
@@ -36,7 +36,7 @@ compute_coverage <- function(score_card) {
       score_card %>%
         mutate(is_below = .data$actual < map_dbl(.data$forecast_distribution, ~ .x$quantiles[i]),
                is_above = .data$actual > map_dbl(.data$forecast_distribution, ~ .x$quantiles[itop]),
-               is_covered = !.data$is_below & !.data$is_above) %>%
+               is_covered = !.data$is_below && !.data$is_above) %>%
         group_by(.data$forecast_date) %>%
         summarize(prop_below = mean(.data$is_below, na.rm = TRUE),
                   prop_above = mean(.data$is_above, na.rm = TRUE),
@@ -53,7 +53,7 @@ compute_coverage <- function(score_card) {
 #' @importFrom assertthat assert_that
 compute_calibration <- function(score_card) {
   probs <- score_card$forecast_distribution[[1]]$probs
-  assert_that(length(probs) %% 2 == 1 & all(abs(probs + rev(probs) - 1) < 1e-3),
+  assert_that(length(probs) %% 2 == 1 && all(abs(probs + rev(probs) - 1) < 1e-3),
               msg="Quantile levels need to be symmetric around 0.5 (and include 0.5).")
   as.list(1:length(probs)) %>%
     map_dfr(function(i) {

52621f578887aefb2123335fee631a30053d774b
jacobbien <jbien@usc.edu>
2020-10-23 01:07:06 -0700

    Combine tests into one (that passes).

--- R-packages/evalcast/tests/testthat/test-basic-functionality-runs-without-error.R
+++ R-packages/evalcast/tests/testthat/test-basic-functionality-runs-without-error.R
@@ -5,7 +5,7 @@ signals <- tibble(data_source = "jhu-csse",
        signal = c("deaths_incidence_num", "confirmed_incidence_num"),
        start_day = "2020-06-15")
 forecast_dates <- ymd(c("2020-07-20", "2020-08-10"))
-test_that("get_predictions on baseline_forecaster works", {
+test_that("get_predictions and evaluate_predictions on baseline_forecaster works", {
   predictions_cards <- get_predictions(baseline_forecaster,
                                        name_of_forecaster = "baseline",
                                        signals = signals,
@@ -14,13 +14,11 @@ test_that("get_predictions on baseline_forecaster works", {
                                        ahead = 3,
                                        geo_type = "state")
   expect_equal(length(predictions_cards), 2)
-})
-
-test_that("evaluate_predictions on baseline_forecaster works", {
   err_measures <- list(wis = weighted_interval_score,
                        ae = absolute_error,
                        coverage_80 = interval_coverage(alpha = 0.2))
-  scorecards <- evaluate_predictions(filter_predictions(predictions_cards, ahead = 3),
+  scorecards <- evaluate_predictions(filter_predictions(predictions_cards,
+                                                        ahead = 3),
                                      err_measures,
                                      backfill_buffer = 10)
 })

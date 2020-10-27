432702a4f15983126d3dc6a00070830af909f2b5
Sarah Colquhoun <scolquho@andrew.cmu.edu>
2020-10-27 10:39:54 -0400

    Basic functionality of allowing non quantile forecasters.

--- R-packages/evalcast/R/baseline.R
+++ R-packages/evalcast/R/baseline.R
@@ -67,3 +67,49 @@ baseline_forecaster <- function(df,
   dplyr::bind_rows(dat, .id = "ahead") %>%
       dplyr::mutate(ahead = as.integer(ahead))
 }
+
+#' Baseline classification forecaster
+#'
+#' This serves as a template for a non-quantile forecaster.  
+#' It's not intended to be a great forecaster.
+#'
+#' @param df a data frame of the format that is outputted by
+#'   \code{\link[covidcast]{covidcast_signal}}.
+#' @param forecast_date date on which forecasts will be made about some period
+#'   (e.g., epiweek).  For example, if forecast_date is ymd("2020-05-11"),
+#'   incidence_period is "day",  and ahead = 3, then, we'd be making forecasts
+#'   for "2020-05-14".
+#' @template signals-template
+#' @template incidence_period-template
+#' @template ahead-template
+#' @template geo_type-template
+#'
+#' @return A data frame with columns "ahead", "location", "probs", "quantiles".
+#'   The quantiles column gives the probs-quantile of the forecast distribution
+#'   for that location and ahead.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @importFrom lubridate ymd
+#' @importFrom dplyr filter group_by arrange mutate select group_by group_modify ungroup bind_rows lag
+#' @importFrom tibble tibble
+#' @importFrom zoo rollsum
+#' @importFrom stats quantile
+#' @export
+
+baseline_classification_forecaster <- function(df,
+                                               forecast_date,
+                                               signals,
+                                               incidence_period = c("epiweek", "day"),
+                                               ahead = 1,
+                                               geo_type){
+  incidence_period <- match.arg(incidence_period)
+  forecast_date <- lubridate::ymd(forecast_date)
+  target_period <- get_target_period(forecast_date, incidence_period, ahead)
+  incidence_length <- ifelse(incidence_period == "epiweek", 7, 1)
+
+  dat <- list()
+  for (a in ahead) {
+    # recall the first row of signals is the response
+    df_test = 
+                                               }

b800d3bb9f17c784a219fb9313b6d415e1506db3
jacobbien <jbien@usc.edu>
2020-10-30 22:09:11 -0700

    Add test that additional arguments to forecaster works.

--- R-packages/evalcast/tests/testthat/test-basic-functionality-runs-without-error.R
+++ R-packages/evalcast/tests/testthat/test-basic-functionality-runs-without-error.R
@@ -4,7 +4,10 @@ signals <- tibble(data_source = "jhu-csse",
                   signal = c("deaths_incidence_num", "confirmed_incidence_num"),
                   start_day = "2020-06-15")
 forecast_dates <- ymd(c("2020-07-20", "2020-08-10"))
+
 test_that("get_predictions and evaluate_predictions on baseline_forecaster works", {
+  # in addition to making sure these functions run, we will also use
+  # the generated pc and sc in other tests.
   pc <<- get_predictions(baseline_forecaster,
                          name_of_forecaster = "baseline",
                          signals = signals,
@@ -29,4 +32,40 @@ test_that("backfill_buffer works", {
                "backfill_buffer")
   # waited long enough:
   evaluate_predictions(pc, backfill_buffer = days_elapsed - 1)
+})
+
+test_that("get_predictions works when forecaster has additional arguments", {
+  forecaster_with_args <- function(df,
+                                   forecast_date,
+                                   signals,
+                                   incidence_period,
+                                   ahead,
+                                   geo_type,
+                                   symmetrize = TRUE,
+                                   arg1,
+                                   arg2) {
+    # this forecaster adds arg1 * arg2 to the predictions of the baseline 
+    # forecaster
+    out <- baseline_forecaster(df = df,
+                               forecast_date = forecast_date,
+                               signals = signals,
+                               incidence_period = incidence_period,
+                               ahead = ahead,
+                               geo_type = geo_type,
+                               symmetrize = symmetrize)
+    out$quantiles <- out$quantiles + arg1 * arg2
+    out
+  }
+  pc2 <- get_predictions(forecaster_with_args,
+                         name_of_forecaster = "forecaster_with_args",
+                         signals = signals,
+                         forecast_dates = forecast_dates,
+                         incidence_period = "epiweek",
+                         ahead = 3,
+                         geo_type = "state",
+                         arg1 = 2,
+                         arg2 = 5)
+  baseline_pred <- pc[[1]]$forecast_distribution[[1]]$quantiles
+  with_args_pred <- pc2[[1]]$forecast_distribution[[1]]$quantiles
+  expect_equal(baseline_pred + 10, with_args_pred)
 })

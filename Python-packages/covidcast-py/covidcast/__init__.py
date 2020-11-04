1275a0e0f3a6212985b2c3c95406554b6befe98e
andrew <chinandrew96@gmail.com>
2020-11-03 16:08:27 -0800

    Move agg funcs to new file

--- Python-packages/covidcast-py/covidcast/__init__.py
+++ Python-packages/covidcast-py/covidcast/__init__.py
@@ -12,7 +12,8 @@ Functions:
 
 """
 
-from .covidcast import signal, metadata, aggregate_signals
+from .covidcast import signal, metadata
+from .wrangle import aggregate_signals
 from .plotting import plot, plot_choropleth, get_geo_df, animate
 from .geography import (fips_to_name, cbsa_to_name, abbr_to_name,
                         name_to_abbr, name_to_cbsa, name_to_fips,

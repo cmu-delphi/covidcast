48680499f2fa1e8e9f440a383d252922ebac74d3
Ryan Tibshirani <ryantibs@gmail.com>
2020-11-25 23:29:14 -0500

    Estimate deriv function

---
+++ R-packages/modeltools/R/modeltools.R
@@ -0,0 +1,9 @@
+#' modeltools: Tools for Building COVID Forecasters and Related Models 
+#'
+#' The modeltools package provides various tools for building COVID-19 models,
+#' mainly forecasting and hotspot prediction models. It is intended to be used
+#' in conjuction with the covidcast and evalcast packages.
+#'
+#' @docType package
+#' @name modetools
+NULL

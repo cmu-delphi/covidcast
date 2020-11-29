1da8460bde958a313c29389f914b720e1c4ccf0c
Ryan Tibshirani <ryantibs@gmail.com>
2020-11-28 23:16:51 -0500

    Major refactor, pct change

--- R-packages/modeltools/R/utils.R
+++ R-packages/modeltools/R/utils.R
@@ -1,12 +1,27 @@
-Start = function(x) head(x, 1)
-End = function(x) tail(x, 1)
+#' Wrappers to functions that remove `NA` values by default
+#' @export
 Min = function(x) min(x, na.rm = TRUE)
+
+#' @rdname Min
+#' @export
 Max = function(x) max(x, na.rm = TRUE)
+
+#' @rdname Min
+#' @export
 Sum = function(x) sum(x, na.rm = TRUE)
+
+#' @rdname Min
+#' @export
 Mean = function(x) mean(x, na.rm = TRUE)
+
+#' @rdname Min
+#' @export
 Median = function(x) median(x, na.rm = TRUE)
 
 ##########
+
+Start = function(x) head(x, 1)
+End = function(x) tail(x, 1)
 
 quiet = function(x) { 
   sink(tempfile()) 

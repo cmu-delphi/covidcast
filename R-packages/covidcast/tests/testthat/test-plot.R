ce6d36028e469eacdecbcac3ab1c1ef81d41e528
andrew <chinandrew96@gmail.com>
2020-12-01 16:45:18 -0800

    oops

--- R-packages/covidcast/tests/testthat/test-plot.R
+++ R-packages/covidcast/tests/testthat/test-plot.R
@@ -59,7 +59,7 @@ test_that("state line graphs", {
 
 test_that("simple state choropleths", {
   fb_state <- readRDS(test_path("data/survey-data-state.rds"))
-  svg("ci-default-state-choropleth-with-include.svg")
+  svg(test_path("ci-default-state-choropleth-with-include.svg"))
   plot(fb_state, plot_type = "choro",
        include = c("pa", "OH", "in", "KY"))
   dev.off() 

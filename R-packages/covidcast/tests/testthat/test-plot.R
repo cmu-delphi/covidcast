7267252c2bdd12b84dce9a63681dce35cc4b7116
andrew <chinandrew96@gmail.com>
2020-12-02 11:09:39 -0800

    use 3.6

--- R-packages/covidcast/tests/testthat/test-plot.R
+++ R-packages/covidcast/tests/testthat/test-plot.R
@@ -59,10 +59,10 @@ test_that("state line graphs", {
 
 test_that("simple state choropleths", {
   fb_state <- readRDS(test_path("data/survey-data-state.rds"))
-  write_svg(plot(fb_state, plot_type = "choro",
-       include = c("pa", "OH", "in", "KY")), "ci.svg")
-  
-  expect_equal(1,1)
+
+  expect_doppelganger("default state choropleth with include",
+                      plot(fb_state, plot_type = "choro",
+                           include = c("pa", "OH", "in", "KY")))
   
 })
 

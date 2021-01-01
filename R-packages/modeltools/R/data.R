44903e66c6d2befd8af8369c09d307dbeec2e1d3
Ryan Tibshirani <ryantibs@gmail.com>
2020-12-31 21:57:41 -0500

    Add covidhub_probs data

---
+++ R-packages/modeltools/R/data.R
@@ -0,0 +1,8 @@
+#' Probabilities (quantile levels) for COVID Hub forecasts
+#' 
+#' Probabilities (quantile levels) for forecasts in COVID Hub format: 0.01,
+#' 0.025, then evenly spaced from 0.05 to 0.95 in increments of 0.05, then
+#' 0.975, 0.99.
+#' 
+#' @format Numeric vector with 23 entries.
+"covidhub_probs"

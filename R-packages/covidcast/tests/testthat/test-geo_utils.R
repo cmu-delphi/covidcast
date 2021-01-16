9ece0521e61c59d405c664681ce9a51f8d2a43a3
Alex Reinhart <alex@refsmmat.com>
2021-01-16 13:00:54 -0500

    Add additional tests of geo lookup behavior
    Specifically behavior in the presence of duplicate keys and multiple
    matches, which could be affected by the new deduplication code.

--- R-packages/covidcast/tests/testthat/test-geo_utils.R
+++ R-packages/covidcast/tests/testthat/test-geo_utils.R
@@ -1,5 +1,3 @@
-suppressPackageStartupMessages(library(covidcast))
-
 test_that("name_to_fips", {
   # Basic function
   expect_equal(name_to_fips("Allegheny"),
@@ -23,4 +21,23 @@ test_that("fips_to_name", {
                c("42003" = "Allegheny County"))
   expect_equal(fips_to_name("42000"),
                c("42000" = "Pennsylvania"))
+
+  # Repeats in vector
+  expect_equal(fips_to_name(c("42003", "42003")),
+               c("42003" = "Allegheny County",
+                 "42003" = "Allegheny County"))
+
+  # Request all ties
+  expect_equal(fips_to_name("4213", ties_method = "all"),
+               list(c("42131" = "Wyoming County",
+                      "42133" = "York County")))
+
+  # Correct matching of multiple ties
+  expect_equal(fips_to_name(c("4213", "4019"), ties_method = "all"),
+               list(c("42131" = "Wyoming County",
+                      "42133" = "York County"),
+                    c("04019" = "Pima County",
+                      "24019" = "Dorchester County",
+                      "34019" = "Hunterdon County",
+                      "54019" = "Fayette County")))
 })

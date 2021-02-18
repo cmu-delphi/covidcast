81f10bab635a09a59bc636261b0855a74d58d05a
Mike O'Brien <mikeob@google.com>
2021-02-18 16:32:51 -0500

    fix coverage plot issues

--- R-packages/evalcast/R/plot_coverage.R
+++ R-packages/evalcast/R/plot_coverage.R
@@ -1,8 +1,15 @@
 
 #' Plot interval coverage
 #'
-#' @param scorecards List of different score cards, all on the same forecasting
-#'   task (i.e., same ahead, etc.).
+#' @param predictions_cards tibble of predictions
+#'   that are all for the same prediction task, meaning they are for the same
+#'   response, incidence period,and geo type. Forecasts may be for a
+#'   different forecast date or forecaster.
+#'   A predictions card may be created by the function
+#'   [get_predictions()], downloaded with [get_covidhub_predictions()] or
+#'   possibly created manually.
+#' @param backfill_buffer How many days until response is deemed trustworthy
+#'   enough to be taken as correct?
 #' @param type One of "all" or "none", indicating whether to show coverage
 #'   across all nominal levels (in which case averaging is performed across
 #'   `avg_vars`) or whether to show it for one specific alpha
@@ -16,15 +23,26 @@
 #' @param legend_position Legend position, the default being "bottom".
 #' 
 #' @export 
-plot_coverage <- function(scorecards, 
+plot_coverage <- function(predictions_cards,
+                          geo_type,
+                          backfill_buffer = 10,
                           type = c("all", "one"), 
                           grp_vars = c("forecaster", "forecast_date", "ahead"),
                           avg_vars = c("geo_value"),
                           coverage = 0.8, 
                           legend_position = "bottom") {
   type <- match.arg(type)
-  # make sure scorecards are comparable:
+
+  scorecards <- left_join(predictions_cards,
+                          get_covidcast_data(predictions_cards,
+                                             backfill_buffer,
+                                             geo_type),
+                          by = c("geo_value",
+                                 "forecast_date",
+                                 "ahead"))
   
+  # make sure scorecards are comparable:
+    
   if (type == "all") {
     grps <- grp_processing_for_facets(scorecards, grp_vars, 4, 
                                       "`all` coverage plots")
@@ -40,17 +58,17 @@ plot_coverage <- function(scorecards,
     
     
     if (ngrps == 3L) {
-      g <- g + geom_line(aes(color = !!sym(grps[1]))) +
+      g <- g + geom_line(aes(color = as.factor(!!sym(grps[1])))) +
         scale_color_viridis_d() +
         facet_grid(stats::as.formula(paste(grps[2:3], collapse = "~")))
     }
     if (ngrps == 2L) {
-      g <- g + geom_line(aes(color = !!sym(grps[1]))) +
+      g <- g + geom_line(aes(color = as.factor(!!sym(grps[1])))) +
         scale_color_viridis_d() +
         facet_wrap(stats::as.formula(paste0("~", grps[2])))
     }
     if (ngrps == 1L) {
-      g <- g + geom_line(aes(color = !!sym(grps)))
+      g <- g + geom_line(aes(color = as.factor(!!sym(grps))))
     }
     if (ngrps == 0L) {
       g <- g + geom_line(color = "orange") + geom_point(color="orange")
@@ -67,12 +85,12 @@ plot_coverage <- function(scorecards,
       summarize(prop_covered = mean(.data$prop_covered, na.rm = TRUE)) %>%
       ggplot(aes(x = !! sym(grps[1]),
                  y = .data$prop_covered)) +
-      geom_hline(yintercept = 1 - alpha, lty = 2) +
+      geom_hline(yintercept = 1 - coverage, lty = 2) +
       labs(x = grps[1], y = "Empirical coverage")
     if (ngrps > 1L) {
       g <- g + 
-        geom_line(aes(color = !!sym(grps[2]))) +
-        geom_point(aes(color = !!sym(grps[2]))) +
+        geom_line(aes(color = as.factor(!!sym(grps[2])))) +
+        geom_point(aes(color = as.factor(!!sym(grps[2])))) +
         scale_color_viridis_d()
     }
     if (ngrps == 4L) {

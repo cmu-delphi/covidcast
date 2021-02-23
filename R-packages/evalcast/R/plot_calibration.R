e6c17b371e90a0c541267084f02b1bddf5ab5829
Sarah Colquhoun <scolquho@andrew.cmu.edu>
2021-02-22 19:22:39 -0500

    Factor out wedgeplot and traditional calibration plot formatting to be able to specify default facet values.  Use as.factor on color to deal with dates as continuous values.

--- R-packages/evalcast/R/plot_calibration.R
+++ R-packages/evalcast/R/plot_calibration.R
@@ -15,96 +15,22 @@ plot_calibration <- function(predictions_cards,
                              type = c("wedgeplot", "traditional"),
                              grp_vars = c("forecaster", "forecast_date", "ahead"),
                              avg_vars = c("geo_value"),
-                             legend_position = "bottom") {
+                             legend_position = "bottom",
+                             ...) {
   
   type <- match.arg(type)
-  
-  scorecard <- left_join(predictions_cards,
-                         get_covidcast_data(predictions_cards,
-                                            backfill_buffer,
-                                            geo_type),
-                         by = c("geo_value",
-                                "forecast_date",
-                                "ahead"))
 
+  calib <- compute_calibration(predictions_cards,
+                               geo_type,
+                               grp_vars,
+                               avg_vars)
+  
   if (type == "wedgeplot") {
-    grps <- grp_processing_for_facets(scorecard, grp_vars, 3, "wedgeplots")
-    
-    if (length(grps) == 1) {
-      facet_cols <- c(grps[1])
-      facet_rows <- NULL
-    } else if (length(grps) == 2) {
-      facet_cols <- c(grps[2]) 
-      facet_rows <- c(grps[1])
-    } else {
-      facet_rows <- NULL
-      facet_cols <- NULL
-    }
-    
-    facet_layer <- facet_grid(rows = vars(!!!syms(facet_rows)),
-                              cols = vars(!!!syms(facet_cols)))
-    
-    calib <- compute_calibration(predictions_cards,
-                                 geo_type,
-                                 grp_vars,
-                                 avg_vars) %>% setup_wedgeplot()
-
-    g <- calib %>%
-      ggplot(aes(x = .data$nominal_prob,
-                 y = .data$proportion,
-                 colour = .data$coverage_type)) +
-      geom_line(aes(alpha = .data$emph, size = .data$emph)) +
-      scale_color_manual(values = c("blue","orange"), name="") +
-      geom_point(aes(alpha = .data$emph, size = .data$emph)) +
-      geom_segment(aes(x = 0, y = 0, xend = 0.5, yend = 0.5), 
-                   size = 0.25, color = "black") +
-      geom_segment(aes(x = 0.5, y = 0.5, xend = 1, yend = 0), 
-                   size = 0.25, color = "black") +
-      labs(x = "Nominal quantile level",
-           y = "Proportion",
-           title = sprintf("%s (ahead = %s): Proportion above/below", 
-                           .data$name, .data$ahead)) +
-      scale_alpha_continuous(range = c(0.5, 1)) +
-      scale_size_continuous(range = c(0.5, 1)) +
-      guides(alpha = FALSE, size = FALSE) + 
-      facet_layer
-    
+    g <- format_wedgeplot(calib, grp_vars, ...)
   } else if (type == "traditional") {
-    grps <- grp_processing_for_facets(scorecard, grp_vars, 4, 
-                                      "traditional calibration plots")
-  
-    if (length(grps) == 2) {
-      facet_cols <- c(grps[2])
-      facet_rows <- NULL
-    } else if (length(grps) == 3) {
-      facet_cols <- c(grps[3]) 
-      facet_rows <- c(grps[2])
-    } else {
-      facet_rows <- NULL
-      facet_cols <- NULL
-    }
-    
-    facet_layer <- facet_grid(rows = vars(!!!syms(facet_rows)),
-                              cols = vars(!!!syms(facet_cols)))
-    color_vars <- setdiff(grp_vars, c(facet_rows, facet_cols))
-    
-    calib <- compute_calibration(predictions_cards,
-                                 geo_type,
-                                 grp_vars,
-                                 avg_vars)
-    
-    g <- calib %>%
-      mutate(color = Interaction(!!!syms(color_vars))) %>%
-      ggplot(aes(x = .data$nominal_prob, y = .data$prop_below)) +
-      geom_abline(slope = 1, intercept = 0) +
-      labs(x = "Quantile level",
-           y = "Proportion",
-           title = sprintf("%s (ahead %s): Calibration", 
-                           .data$name, .data$ahead)) +
-      geom_line(aes(color = .data$color, group = .data$color)) +
-      scale_color_viridis_d() +
-      facet_layer 
+    g <- format_traditional_calib_plot(calib, grp_vars, ...)
   }
+  
   return(
     g + xlim(0, 1) + ylim(0, 1) + theme_bw() + 
     theme(legend.position = legend_position))
@@ -128,4 +54,109 @@ setup_wedgeplot <- function(calib) {
     mutate(coverage_type = recode(.data$coverage_type,
                                   prop_above = "Proportion above",
                                   prop_below = "Proportion below"))
+}
+
+
+#' ggplot formatting for wedge calibration plot.
+#'
+#' @param calib the output from [compute_calibration()]
+#' @param grp_vars variables over which to compare calibration. These
+#'   determines the color of the lines and faceting depending on `type`
+#' @param facet_rows A variable name to facet data over. Creates a
+#'   separate row of plots for each value of specified variable. Can be used
+#'   with `facet_cols` to create a grid of plots.  Should be passed to 
+#'   plot_calibration when customized.
+#' @param facet_cols Same as `facet_rows`, but with columns.
+#'
+#' @return ggplot object
+format_wedgeplot <- function(calib, 
+                             grp_vars,
+                             facet_rows = "forecast_date",
+                             facet_cols = "forecaster"){
+  if (!is.null(facet_rows)) {
+    non_grouped_facet <- setdiff(facet_rows, grp_vars)
+    assert_that(length(non_grouped_facet) == 0,
+                msg = paste("Variables must be grouped in order to be faceted in rows:",
+                            non_grouped_facet))
+  }
+  if (!is.null(facet_cols)) {
+    non_grouped_facet <- setdiff(facet_cols, grp_vars)
+    assert_that(length(non_grouped_facet) == 0,
+                msg = paste("Variables must be grouped in order to be faceted in cols:",
+                            non_grouped_facet))
+  }
+  
+  facet_layer <- facet_grid(rows = vars(!!!syms(facet_rows)),
+                            cols = vars(!!!syms(facet_cols)))
+  g <- calib %>%
+    setup_wedgeplot() %>%
+    ggplot(aes(x = .data$nominal_prob,
+               y = .data$proportion,
+               colour = .data$coverage_type)) +
+    geom_line(aes(alpha = .data$emph, size = .data$emph)) +
+    scale_color_manual(values = c("blue","orange"), name="") +
+    geom_point(aes(alpha = .data$emph, size = .data$emph)) +
+    geom_segment(aes(x = 0, y = 0, xend = 0.5, yend = 0.5), 
+                 size = 0.25, color = "black") +
+    geom_segment(aes(x = 0.5, y = 0.5, xend = 1, yend = 0), 
+                 size = 0.25, color = "black") +
+    labs(x = "Nominal quantile level",
+         y = "Proportion",
+         title = sprintf("%s (ahead = %s): Proportion above/below", 
+                         .data$name, .data$ahead)) +
+    scale_alpha_continuous(range = c(0.5, 1)) +
+    scale_size_continuous(range = c(0.5, 1)) +
+    guides(alpha = FALSE, size = FALSE) +
+    facet_layer
+  
+  return(g)
+}
+
+#' ggplot formatting for traditional calibration plot.
+#'
+#' @param calib the output from [compute_calibration()]
+#' @param grp_vars variables over which to compare calibration. These
+#'   determines the color of the lines and faceting depending on `type`
+#' @param facet_rows A variable name to facet data over. Creates a
+#'   separate row of plots for each value of specified variable. Can be used
+#'   with `facet_cols` to create a grid of plots.  Should be passed to 
+#'   plot_calibration when customized.
+#' @param facet_cols Same as `facet_rows`, but with columns.
+#'
+#' @return ggplot object
+format_traditional_calib_plot <- function(calib, 
+                                          grp_vars,
+                                          facet_rows = "ahead",
+                                          facet_cols = "forecaster"){
+  if (!is.null(facet_rows)) {
+    non_grouped_facet <- setdiff(facet_rows, grp_vars)
+    assert_that(length(non_grouped_facet) == 0,
+                msg = paste("Variables must be grouped in order to be faceted in rows:",
+                            non_grouped_facet))
+  }
+  if (!is.null(facet_cols)) {
+    non_grouped_facet <- setdiff(facet_cols, grp_vars)
+    assert_that(length(non_grouped_facet) == 0,
+                msg = paste("Variables must be grouped in order to be faceted in cols:",
+                            non_grouped_facet))
+  }
+  
+  facet_layer <- facet_grid(rows = vars(!!!syms(facet_rows)),
+                            cols = vars(!!!syms(facet_cols)))
+  
+  color_vars <- setdiff(grp_vars, c(facet_rows, facet_cols))
+  
+  g <- calib %>%
+    mutate(color = as.factor(Interaction(!!!syms(color_vars)))) %>%
+    ggplot(aes(x = .data$nominal_prob, y = .data$prop_below)) +
+    geom_abline(slope = 1, intercept = 0) +
+    labs(x = "Quantile level",
+         y = "Proportion",
+         title = sprintf("%s (ahead %s): Calibration", 
+                         .data$name, .data$ahead)) +
+    geom_line(aes(color = .data$color, group = .data$color)) +
+    scale_color_viridis_d() +
+    facet_layer 
+  
+  return(g)
 }

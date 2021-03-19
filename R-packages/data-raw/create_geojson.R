002ea2fa3ca942ad50a52d7b04a11007add73eea
statsmaths <tbarnold@protonmail.ch>
2021-03-19 11:19:01 -0400

    Adding code to create simplified geojson files

---
+++ R-packages/data-raw/create_geojson.R
@@ -0,0 +1,73 @@
+library(ggplot2)
+library(sf)
+library(dplyr)
+
+simplify_shape <- function(shp_obj, crs, plot_name = NULL)
+{
+  # Transform to desired CRS
+  shp_simple <- st_transform(shp_obj, crs)
+
+  # Remove small parts of polygons (islands)
+  num_polygon <- sapply(st_geometry(shp_simple), length)
+  for ( i in which(num_polygon > 1) )
+  {
+    z <- st_cast(shp_simple$geometry[i], "POLYGON")
+    areas <- as.numeric(st_area(z)) / 10000
+    index <- which((areas >= 0.05 * sum(areas)) | (areas == max(areas)))
+    z <- z[index]
+    shp_simple$geometry[i] <- st_union(z)
+  }
+
+  # Simplify and validate
+  shp_simple <- st_simplify(
+    shp_simple, preserveTopology = TRUE, dTolerance = 10000
+  )
+  shp_simple <- st_make_valid(shp_simple)
+
+  # Plot to verify
+  if (!is.null(plot_name))
+  {
+    dir.create("temp", FALSE)
+    p <- ggplot(shp_simple) + geom_sf(size = 0.1) + theme_void()
+    ggsave(file.path("temp", sprintf("%s.png", plot_name)), p)
+  }
+
+  # Back to Lat/Lon
+  shp_simple <- st_transform(shp_simple, 4326)
+
+  return(shp_simple)
+}
+
+save_shp <- function(shp_obj, map_name)
+{
+  # Save file locally as geojson
+  if (file.exists(fe <- sprintf("%s.geojson.bz2", map_name)))
+  {
+    file.remove(fe)
+  }
+  st_write(shp_simple, sprintf("%s.geojson", map_name))
+  system(sprintf("bzip2 %s.geojson", map_name))
+
+  # Create compressed geojson file and print out file size
+  fname <- sprintf("%s.geojson.bz2", map_name)
+  oname <- file.path("../covidcast/inst/shapefiles", fname)
+  cat(sprintf(
+    "File size %s: %0.2f M\n",
+    map_name,
+    file.size(sprintf("%s.geojson.bz2", map_name)) / 1e6
+  ))
+
+  # Put the file in the covidcast package
+  if (file.exists(oname)) { file.remove(oname) }
+  file.rename(fname, oname)
+}
+
+# Run the functions above for the four geographies; Note, the projection is not
+# great for the U.S. territories, but appears to work OK for simplification
+for (map_name in c("msa", "state", "hrr"))
+{
+  # Simplfy shapes using U.S. centered Albers (CRS => 5070)
+  shp_obj <- read_sf(file.path("geo", map_name))
+  shp_simple <- simplify_shape(shp_obj, 5070, map_name)
+  save_shp(shp_simple, map_name)
+}
--- R-packages/data-raw/create_rda.R
+++ R-packages/data-raw/create_rda.R
@@ -11,9 +11,9 @@ county_census$CTYNAME = iconv(county_census$CTYNAME, "latin1", "UTF-8")
 Encoding(county_census$CTYNAME) = "UTF-8"
 
 # Generate 5 digit fips column and drop unused columns
-county_census = county_census %>% 
+county_census = county_census %>%
   mutate(
-    FIPS = str_c(str_pad(pull(county_census, STATE), 2, pad = "0"), 
+    FIPS = str_c(str_pad(pull(county_census, STATE), 2, pad = "0"),
                  str_pad(pull(county_census, COUNTY), 3, pad = "0"))
   )
 county_census = county_census %>% select(SUMLEV, REGION, DIVISION, STATE, COUNTY, STNAME, CTYNAME, POPESTIMATE2019, FIPS)
@@ -22,8 +22,8 @@ county_census = county_census %>% select(SUMLEV, REGION, DIVISION, STATE, COUNTY
 # from https://www.census.gov/data/tables/2010/dec/2010-island-areas.html,
 # https://www.census.gov/data/tables/time-series/demo/popest/2010s-total-puerto-rico-municipios.html,
 # and http://datadictionary.naaccr.org/default.aspx?c=11&Version=21#Virgin%20Islands%20of%20the%20United%20States
-load("./county_territories.rda")
-load("./county_pr.rda")
+load("./metadata/county_territories.rda")
+load("./metadata/county_pr.rda")
 county_census = rbind(county_census, county_pr, county_territories)
 save(county_census, file = "../covidcast/data/county_census.rda", compress = "bzip2")
 
@@ -35,6 +35,8 @@ msa_census$CBSA = as.character(msa_census$CBSA)
 msa_census$NAME = iconv(msa_census$NAME, "latin1", "UTF-8")
 Encoding(msa_census$NAME) = "UTF-8"
 msa_census$STATE = substring(msa_census$NAME, regexpr(",", msa_census$NAME) + 2)
+msa_census <- msa_census %>% select(CBSA, LSAD, MDIV, NAME, POPESTIMATE2019, STATE, STCOU)
+
 save(msa_census, file = "../covidcast/data/msa_census.rda")
 
 # State census data
@@ -48,7 +50,7 @@ state_abbr["Puerto Rico Commonwealth"] = "PR"
 state_census$ABBR = state_abbr
 
 # Add territories data like we did for county_census
-load("./state_territories.rda")
+load("./metadata/state_territories.rda")
 state_census = rbind(state_census, state_territories)
 save(state_census, file = "../covidcast/data/state_census.rda")
 
@@ -57,10 +59,10 @@ county_col_classes = c("numeric", "numeric", "character", "character",
                        "character", "character")
 county_file = system.file("extdata", "us_counties_centroids.csv", package = "usmap")
 county_geo = utils::read.csv(county_file, colClasses = county_col_classes)
-save(county_geo, file = "../covidcast/data/county_geo.rda", compress = "bzip2")
+#save(county_geo, file = "../covidcast/data/county_geo.rda", compress = "bzip2")
 
 # State geo centroids from usmap
 state_col_classes = c("numeric", "numeric", "character", "character", "character")
 state_file = system.file("extdata", "us_states_centroids.csv", package = "usmap")
 state_geo = utils::read.csv(state_file, colClasses = state_col_classes)
-save(state_geo, file = "../covidcast/data/state_geo.rda")
+#save(state_geo, file = "../covidcast/data/state_geo.rda")

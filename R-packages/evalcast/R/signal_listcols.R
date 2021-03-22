81b8aabf2a08d530369ecaf46de61398fafb3b92
Daniel J. McDonald <dajmcdon@gmail.com>
2021-03-22 08:20:48 -0700

    address some of @sgsmob comments. The test is still odd.

--- R-packages/evalcast/R/signal_listcols.R
+++ R-packages/evalcast/R/signal_listcols.R
@@ -1,3 +1,7 @@
+# In the signals tibble, required by `get_predictions()`,
+# start_day` and `as_of` can possibly be functions
+# of the forecast_date. Here we evaluate those functions as well as creating
+# default values for `geo_values` and `geo_type` if necessary
 signal_listcols <- function(signals, forecast_date) {
   
   cn <- colnames(signals)
@@ -13,15 +17,18 @@ signal_listcols <- function(signals, forecast_date) {
     signals$start_day <- std
   }
   
-  if (! "as_of" %in% cn) signals$as_of <- forecast_date
-  if ("as_of" %in% cn && is.list(signals$as_of)) {
-    as_of <- Date(ns)
-    for (i in 1:length(signals$as_of)) {
-      if (is.function(signals$as_of[[i]])) {
-        as_of[i] <- as.Date(signals$as_of[[i]](forecast_date))
+  if (! "as_of" %in% cn) {
+    signals$as_of <- forecast_date
+  } else {
+    if (is.list(signals$as_of)) {
+      as_of <- Date(ns)
+      for (i in 1:length(signals$as_of)) {
+        if (is.function(signals$as_of[[i]])) {
+          as_of[i] <- as.Date(signals$as_of[[i]](forecast_date))
+        }
       }
+      signals$as_of <- as_of
     }
-    signals$as_of <- as_of
   }
   
   if (! "geo_values" %in% cn) signals$geo_values <- "*"

75dc1b4dba1fa22e9cfe0b57aa21cef709fb6812
Mike O'Brien <mikeob@google.com>
2021-03-25 10:58:38 -0400

    move values to arguments

--- R-packages/modeltools/R/example_forecaster.R
+++ R-packages/modeltools/R/example_forecaster.R
@@ -1,7 +1,12 @@
 #' Simple example forecaster that forecasts confirmed cases.
 #'
-#' @param df_list List of downloaded covidcast signals
+#' @param df_list List of downloaded covidcast signals.
 #' @param forecast_date Date from which the forecast should be made.
+#' @param ahead Vector of epiweeks ahead to predict.
+#' @param training_window_size Number of days prior to `forecast_date` to use for training.
+#' @param lags Vector of lags or list of vectors of lags to apply to `df_list`.  See documentation
+#'     of `dt` argument to `covidcast::aggregate_signals()` for details.
+#' @param quantiles Vector of quantile values at which to predict
 #'
 #' preds <- evalcast::get_predictions(modeltools::example_forecaster,
 #'                                    "example",
@@ -14,12 +19,16 @@
 #'                                    as.Date("2021-03-01"),
 #'                                    "epiweek")
 #'
+#' @importFrom dplyr mutate bind_cols, bind_rows
+#' @importFrom tidyr pivot_longer
+#'
 #' @export
-example_forecaster <- function(df_list, forecast_date) {
-    ahead <- 1:4
-    lags <- c(0, -1, -2, -3, -7, -14)
-    quantiles <- modeltools::covidhub_probs()
-
+example_forecaster <- function(df_list,
+                               forecast_date,
+                               ahead = 1:4,
+                               training_window_size = 14,
+                               lags = seq(0, -1*training_window_size, -7),
+                               quantiles = modeltools::covidhub_probs) {
     covariates <- covidcast::aggregate_signals(df_list, dt = lags, format = "wide")
     covariates_with_response <- add_response_columns(covariates,
                                                      "jhu-csse_confirmed_incidence_num",
@@ -29,7 +38,7 @@ example_forecaster <- function(df_list, forecast_date) {
 
     results <- list()
     for (a in ahead) {
-        mats <- create_train_and_predict_matrices(covariates_with_response, a, 14)
+        mats <- create_train_and_predict_matrices(covariates_with_response, a, training_window_size)
         model <- quantgen::quantile_lasso(mats$train_x, mats$train_y, quantiles, lambda = 0)
         predictions <- quantgen:::predict.quantile_genlasso(model, mats$predict_x)
 

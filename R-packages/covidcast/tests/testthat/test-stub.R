1f716fd34c306bbd026de432c0021d87cd5c2ae2
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2023-07-03 15:13:41 -0400

    Failing tests for testing with http caching

---
+++ R-packages/covidcast/tests/testthat/test-stub.R
@@ -0,0 +1,25 @@
+library(httptest)
+library(mockery)
+library(MMWRweek)
+library(dplyr)
+
+with_mock_api({
+  test_that("with_mock_api supports httr::rerequest", {
+    # the first call to covidcast_meta is routed through httr::request
+    mdf1 <- covidcast_meta()
+    
+    # the second call goes through httr::rerequest
+    mdf2 <- covidcast_meta()
+    expect_equals(nrow(mdf2), 3)
+  })
+})
+
+with_mock_api({
+  test_that("mockery::stub takes effect even when httptest::with_mock_api is active", {
+    mock_identical <- mock(TRUE)
+    stub(.request_meta, "identical", mock_identical)
+    covidcast_meta()
+    print(mock_calls(mock_identical))
+    expect_called(mock_identical, 1)
+  })
+})

474dc20ad2f123b5b89d21f8443ee107537fe84e
melange396 <george.haff@gmail.com>
2023-09-22 10:21:45 -0400

    Update Python-packages/covidcast-py/setup.py

--- Python-packages/covidcast-py/setup.py
+++ Python-packages/covidcast-py/setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     install_requires=[
         "pandas<2",
         "requests",
-        "delphi-epidata>=4.1.2",
+        "delphi-epidata>=4.1.10",
         "geopandas",
         "matplotlib",
         "numpy",
